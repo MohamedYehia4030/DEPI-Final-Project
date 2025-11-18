650662764ba6502f0332d8e440a104bec98b79b4
Mohamed Talal <medoalashkam5@gmail.com>
2025-11-18 02:15:31 +0200

    created the btton.jsx, the auth components and the stateMangment using Zustand

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,10 +3,12 @@ import Navbar from '../layout/Navbar/Navbar';
 import Footer from '../layout/Footer/Footer';
 import AppRoutes from './routes';
 import './App.css';
+import AuthModal from '../features/auth/components/AuthModal';
 
 function App() {
   return (
     <>
+    <AuthModal />
       <Navbar />
       <main>
         <AppRoutes />
--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,12 @@
-// This is where you would set up your global state (e.g., Redux, Zustand, Context)
-// For example, to control the AuthModal
-
-// Example using Zustand (npm install zustand)
-/*
 import { create } from 'zustand';
 
-export const useAppStore = create((set) => ({
+const useAppStore = create((set) => ({
   isAuthModalOpen: false,
-  openAuthModal: () => set({ isAuthModalOpen: true }),
+  authView: null,
+  
+  openAuthModal: (view) => set({ isAuthModalOpen: true, authView: view }),
   closeAuthModal: () => set({ isAuthModalOpen: false }),
 }));
-*/
 
-// Placeholder
-const store = {};
-export default store;
+
+export default useAppStore;
