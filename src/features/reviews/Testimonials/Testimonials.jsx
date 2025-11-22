a6b391c13682cbd68fa343f8c8fb1307fa69d12c
Mohamed Talal <medoalashkam5@gmail.com>
2025-11-22 19:29:45 +0200

    deleted Testimonials file

--- src/features/reviews/Testimonials/Testimonials.jsx
+++
@@ -1,7 +0,0 @@
-// This file was previously the Testimonials component implementation.
-// The component was moved to `src/components/Testimonials/Testimonials.jsx`.
-// Keep a lightweight re-export here so older imports continue to work during migration.
-
-import Testimonials from '../../../components/Slider/Slider';
-
-export default Testimonials;
