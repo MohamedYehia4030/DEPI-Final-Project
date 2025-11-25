749c0c771dfa1e6f29aebe552240c2ac4c63da71
Mohamed Talal <medoalashkam5@gmail.com>
2025-11-25 20:09:33 +0200

    pulled changes and pushed

--- src/features/screens/homeScreen/HomeScreen.jsx
+++ src/features/screens/homeScreen/HomeScreen.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import HeroSection from '../../home/components/HeroSection';
-import PopularDestinations from '../../home/components/PopularDestinations';
-import ExploreCategories from '../../home/components/ExploreCategories';
-import PopularTransport from '../../home/components/PopularTransport';
-import TravelTips from '../../home/components/TravelTips';
-import OffersCTA from '../../home/components/OffersCTA';
-import Testimonials from '../../../components/Testimonials/Testimonials';
-import BikeBookingForm from '../../../components/BikeBookingForm/BikeBookingForm.jsx';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import HeroSection from "../../home/components/HeroSection";
+import PopularDestinations from "../../home/components/PopularDestinations";
+import ExploreCategories from "../../home/components/ExploreCategories";
+import PopularTransport from "../../home/components/PopularTransport";
+import TravelTips from "../../home/components/TravelTips";
+import OffersCTA from "../../home/components/OffersCTA";
+import BikeBookingForm from "../../../components/BikeBookingForm/BikeBookingForm.jsx";
+import styles from "./HomeScreen.module.css";
 
 export default function HomePage() {
-
   return (
-    <div className="HomePage">
+    <div className={styles.HomePage}>
       <HeroSection />
       <PopularDestinations />
       <ExploreCategories />
@@ -20,7 +18,6 @@ export default function HomePage() {
       <BikeBookingForm />
       <TravelTips />
       <OffersCTA />
-   
     </div>
   );
 }
